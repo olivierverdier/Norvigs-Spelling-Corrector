30f8d92ce4564578068f4fefae2662077171688f
Olivier Verdier <olivier.verdier@gmail.com>
2015-10-16 22:48:31 +0200

    falthy -> falsy

--- src/First.hs
+++ src/First.hs
@@ -4,22 +4,22 @@ import Data.Monoid (Monoid(mempty,mappend))
 import Data.Maybe (isNothing)
 
 class BoolLike a where
-    falthy :: a -> Bool
+    falsy :: a -> Bool
     bempty :: a
 
 instance BoolLike [a] where
     bempty = []
-    falthy = null
+    falsy = null
 
 newtype First a = MkFirst { getFirst :: a } deriving (Show, Eq)
 
 instance BoolLike a => Monoid (First a) where
     mempty = MkFirst bempty
-    mappend (MkFirst l) (MkFirst r) = MkFirst $ if falthy l then r else l
+    mappend (MkFirst l) (MkFirst r) = MkFirst (if falsy l then r else l)
 
 
 -- For completeness:
 
 instance BoolLike (Maybe a) where
     bempty = Nothing
-    falthy = isNothing
+    falsy = isNothing
