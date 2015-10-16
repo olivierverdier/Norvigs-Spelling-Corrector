7b8ef3c83f6255428d05169d0e9cc859a9435dc1
Olivier Verdier <olivier.verdier@gmail.com>
2015-10-16 22:48:54 +0200

    general readability improvement

--- src/Spelling.hs
+++ src/Spelling.hs
@@ -4,7 +4,7 @@ module Spelling  where
 
 import Prelude ()
 
-import GHC.Base ((.), ($))
+import GHC.Base ((.))
 import GHC.Num (Num, (+))
 import GHC.Types (Int)
 import Data.Tuple (fst, snd)
@@ -26,7 +26,6 @@ import Data.Text (unpack)
 import Data.Text.IO (readFile)
 import qualified Data.Map.Strict as M
 import Data.Maybe (mapMaybe)
-import Control.Applicative ((<$>))
 import Data.Monoid ((<>))
 import Data.Foldable (Foldable, foldl')
 
@@ -46,7 +45,10 @@ delete [] = []
 delete (_:xs) = xs
 
 insert :: [a] -> [a] -> [[a]]
-insert letters l = (:l) <$> letters
+insert letters word = 
+    do -- List
+      l <- letters
+      return (l:word)
 
 replace :: [a] -> [a] -> [[a]]
 replace _ [] = []
@@ -57,55 +59,68 @@ allEditors letters =  [return . transpose, replace letters, return . delete, ins
 
 
 splits :: [a] -> [([a],[a])]
-splits word = zip (inits word) (tails word)
+splits = 
+    do -- Reader
+      iw <- inits
+      tw <- tails
+      return (zip iw tw)
 
 editsOnceWith :: [[a]->[[a]]] -> [a] -> [[a]]
-editsOnceWith editors word = do
+editsOnceWith editors word = do -- List
     (begin,end) <- splits word
     editor <- editors
     endedit <- editor end
-    return $ begin ++ endedit
+    return (begin ++ endedit)
 
 
-known :: (Eq k, Ord k) => M.Map k v -> [k] -> [(k,v)]
-known dict = mapMaybe myLookup
+inDict :: (Eq k, Ord k) => M.Map k v -> [k] -> [(k,v)]
+inDict dict = mapMaybe myLookup
     where
-      myLookup w = -- returns either Nothing or Just (w,f) if w is found in dict
-        (,) w <$> M.lookup w dict
+      myLookup w = -- returns either Nothing or Just (w,f) if w is found in dict (f is the frequency)
+        -- (,) w <$> M.lookup w dict
+        do -- Maybe
+          f <- M.lookup w dict
+          return (w,f)
 
-choices :: ([a] -> [(a,b)]) -> (a -> [a]) -> a -> [(a,b)]
-choices inDict edits1 word = getFirst $ 
-  mkFirst return
-  <>  mkFirst edits1 
-  <>  mkFirst (edits1 <=< edits1) 
+allChoices :: ([a] -> [(a,b)]) -> (a -> [a]) -> a -> [(a,b)]
+allChoices inDict' edits1 word = getFirst possibilities
   where
-    mkFirst edit = MkFirst . inDict . edit $ word
+    possibilities = 
+      mkFirst return
+      <>  mkFirst edits1 
+      <>  mkFirst (edits1 <=< edits1) 
+    mkFirst edit = (MkFirst . inDict' . edit) word
 
 {- Missing function from the Data.Foldable package: maximumBy with default for
   empty list -}
 
 maxByOrDefault :: (Foldable t) => (a -> a -> Ordering) -> a -> t a -> a
-maxByOrDefault comp = foldl' (\ e e' -> if comp e' e == GT then e' else e)
+maxByOrDefault comp def list = foldl' max' def list
+  where
+    max' e e' = if comp e' e == GT then e' else e
 
 {- Choose word with best score -}
 
 chooseBest :: (Ord k, Ord v, Num v) => k -> [(k,v)] -> k
-chooseBest nothing choices' = fst $ maxByOrDefault (comparing snd) (nothing, 0) choices'
+chooseBest nothing choices' = fst bestPair
+    where bestPair = maxByOrDefault (comparing snd) (nothing, 0) choices'
 
 {- Getting the training dictionary -}
 
 nWords :: IO TrainingDict
-nWords = do
+nWords = do -- IO
   fileName <- getDataFileName "big.txt"
   ws <- readFile fileName
-  return (train . lowerWords . unpack $ ws)
+  return ((train . lowerWords . unpack) ws)
 
 lowerWords :: String -> [String]
 lowerWords = words . fmap normalize
   where normalize c = if isAlpha c then toLower c else ' '
 
 train :: (Ord k, Num v) => [k] -> M.Map k v
-train = foldl' (\ dict x -> M.insertWith (+) x 1 dict) M.empty
+train trainWords = foldl' increment M.empty trainWords
+  where
+    increment dict x = M.insertWith (+) x 1 dict
 
 {- Piecing all together -}
 
@@ -113,9 +128,11 @@ alphabet :: String
 alphabet = ['a' .. 'z']
 
 correct :: TrainingDict -> String -> String -> String
-correct dict notfound word = chooseBest notfound $ choices (known dict) (editsOnceWith $ allEditors alphabet) word
+correct dict notfound word = chooseBest notfound choices
+  where
+    choices = allChoices (inDict dict) (editsOnceWith (allEditors alphabet)) word
 
 ioCorrect :: String -> IO String
-ioCorrect w =  do
+ioCorrect w =  do -- IO
     d <- nWords
-    return $ correct d "" w
+    return (correct d "" w)
